f6c8a973f64f6d0735e232199ebf3a704acdf74f
Martian-Array <wyu571428@gmail.com>
2026-05-19 13:55:07 +0000

    Deploying to gh-pages from @ Martian-Array/Martian-Array.github.io@bd2669d55f07db13ad855665d9ef1023355e37e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-team",
           title: "team",
-          description: "我的团队与实验室",
+          description: "team&amp;lab",
           section: "Navigation",
           handler: () => {
             window.location.href = "/team/";
